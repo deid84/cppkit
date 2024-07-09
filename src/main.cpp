cd9d08a0713083890ed64b773ca2f2cc6f09c9af
deid84 <d.ferrero@me.com>
2024-07-09 12:03:53 +0200

    doxygen sample comment and prints cli arguments

--- src/main.cpp
+++ src/main.cpp
@@ -1,6 +1,27 @@
 #include <iostream>
+#include <string>
+#include <vector>
 #include "cppkit/version.hpp"
 
-int main(int, char**){
+
+/**
+ * @brief Entry point of the application.
+ *
+ * This is the main function of the application. It prints the welcome message
+ * along with the version of cppkit and then lists all the command line 
+ * arguments provided to the application.
+ *
+ * @param argc The number of command line arguments.
+ * @param argv An array of C-style strings representing the command line arguments.
+ * @return int Returns 0 on successful execution.
+ */
+int main(int argc, char* argv[]){
+    std::vector<std::string> args(argv, argv + argc);
+
     std::cout << "welcome to cppkit: " << CPPKIT_VERSION << std::endl;
+    std::cout << "----------" << std::endl;
+ 
+    for (const std::string &arg : args) {
+        std::cout << arg << std::endl;    
+    } 
 }
